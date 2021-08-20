b860b7ff758aaec98e9384d22581c9a992c12e71
Wisniewski <wisniewk@emea.roche.com>
2021-08-20 06:25:40 +0200

    Init project

---
+++ src/main/java/com/example/app/commands/input/InputCommand.java
@@ -0,0 +1,14 @@
+package com.example.app.commands.input;
+
+public class InputCommand {
+		
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/com/example/app/commands/output/OutputCommand.java
@@ -0,0 +1,14 @@
+package com.example.app.commands.output;
+
+public class OutputCommand {
+		
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
